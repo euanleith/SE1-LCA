361dd0f7c01da4d95f5b28c926d6bb0957baade6
Euan Leith <leithe@tcd.ie>
2020-10-18 15:16:29 +0100

    set up unit testing

---
+++ SE1-LCA_cpp/cmake-build-debug/Tests/CMakeFiles/Tests_run.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tests_run.dir/BSTTest.cpp.o"
+  "Tests_run"
+  "Tests_run.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tests_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ SE1-LCA_cpp/cmake-build-debug/src/CMakeFiles/BST.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/BST.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
